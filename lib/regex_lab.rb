3688949652969de1ba6b8099e1e2aa2607808d98
Candice Peters <candicelizabethpeters@gmail.com>
2019-08-14 22:19:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,8 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.scan(/\w/)
+letter = text.scan(/\w/)
+if letter.
 end
 
 def valid_phone_number?(phone)
